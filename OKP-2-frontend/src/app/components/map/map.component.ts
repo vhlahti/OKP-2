6b6b2868595e7efb755d7174576d0b3a8d49931d
vhlahti <henri.veholahti@gmail.com>
2023-03-27 13:32:20 +0300

    add function to fetch user's current location

--- OKP-2-frontend/src/app/components/map/map.component.ts
+++ OKP-2-frontend/src/app/components/map/map.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
-export class MapComponent {
-
-   // google maps settings
-
-   options: google.maps.MapOptions = {
-    center: {lat: 60.172727, lng: 24.939491},
-    zoom: 13
+export class MapComponent implements OnInit {
+  // google maps settings
+  zoom = 13;
+  height = '400px';
+  width = '400px';
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    // additional settings here
   };
 
+  constructor() {}
+
+  ngOnInit(): void {
+    // set helsinki city center as default location
+    this.center = { lat: 60.172727, lng: 24.939491 };
+
+    // get user's current location via geolocation api
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+      });
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+  }
 }
