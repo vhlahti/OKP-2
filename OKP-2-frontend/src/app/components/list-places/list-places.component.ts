5c3228389d9535152bf4875c0430f59df2cd02f6
Miconen <micropizza.mc@gmail.com>
2023-03-31 13:08:41 +0300

    Seperate each api endpoint call to their own components

---
+++ OKP-2-frontend/src/app/components/list-places/list-places.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { PlaceV2 } from 'src/app/models/helsinki-api-model';
+import { APIResponse } from 'src/app/models/IApiResponse';
+
+@Component({
+  selector: 'app-list-places',
+  templateUrl: './list-places.component.html',
+  styleUrls: ['./list-places.component.css']
+})
+export class ListPlacesComponent implements OnInit {
+
+    places: PlaceV2[];
+
+    constructor(private dataService: DataService) { }
+
+    ngOnInit(): void {
+        this.getPlacesData();
+    }
+
+    getPlacesData() {
+        this.dataService.getPlaces().subscribe((res: APIResponse) => {
+            let result = JSON.parse(res.data.result);
+            this.places = result.data;
+        });
+    }
+
+    getPlace(place: PlaceV2) {
+        return place.name.fi ?? place.name.en;
+    }
+}
---
+++ OKP-2-frontend/src/app/models/IApiResponse.ts
@@ -0,0 +1,5 @@
+export interface APIResponse {
+    data: {
+        result: string;
+    }
+}
