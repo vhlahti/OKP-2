404d4602ef1c330aec616ee369374591a161733f
Miconen <micropizza.mc@gmail.com>
2023-04-05 11:29:25 +0300

    Add functionality for tabs

--- OKP-2-frontend/src/app/components/footer/footer.component.ts
+++ OKP-2-frontend/src/app/components/footer/footer.component.ts
@@ -8,10 +8,37 @@ import { faLandmark, faMasksTheater, faPersonBiking } from '@fortawesome/free-so
 })
 export class FooterComponent {
   faLandmark = faLandmark; 
-    faPersonBiking = faPersonBiking;
-    faMasksTheater = faMasksTheater;
+  faPersonBiking = faPersonBiking;
+  faMasksTheater = faMasksTheater;
 
+  placesStatus = "block";
+  activitiesStatus = "none";
+  eventsStatus = "none";
 
-  
+  public clickHandler(label: string) {
+    switch (label) {
+      case "places":
+        this.placesStatus = "block";
+        this.activitiesStatus = "none";
+        this.eventsStatus = "none";
+        break;
+
+      case "activities":
+        this.placesStatus = "none";
+        this.activitiesStatus = "block";
+        this.eventsStatus = "none";
+        break;
+
+      case "events":
+        this.placesStatus = "none";
+        this.activitiesStatus = "none";
+        this.eventsStatus = "block";
+        break;
+    
+      default:
+        break;
+    }
+  }
 }
+
 
