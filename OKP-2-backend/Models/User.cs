4a72dc64e6497adddc9d15df6a55ff91b84d01bb
Miconen <micropizza.mc@gmail.com>
2023-04-06 11:16:59 +0300

    Adding support for login and signup

---
+++ OKP-2-backend/Models/User.cs
@@ -0,0 +1,16 @@
+namespace backend.Models;
+
+public partial class User
+{
+    public User(string name, string password)
+    {
+        Name = name;
+        Password = password;
+    }
+
+    public string Name { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Role { get; set; } = "user";
+}
