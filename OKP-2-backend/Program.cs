58c163c07816bad16d36f03903c31a2e6335e749
Miconen <micropizza.mc@gmail.com>
2023-04-27 14:57:37 +0300

    Add support for new JWT provider service

--- OKP-2-backend/Program.cs
+++ OKP-2-backend/Program.cs
@@ -1,9 +1,16 @@
 using backend.Clients;
 using backend.Models;
+using backend.Auth;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
 string[] origins = { "http://localhost:4200", "https://localhost:4200" };
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", policyBuilder => policyBuilder
@@ -12,10 +19,28 @@ builder.Services.AddCors(options =>
         .AllowAnyHeader()
         .AllowCredentials());
 });
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = config["JwtSettings:Issuer"],
+        ValidAudience = config["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+    };
+});
+
+builder.Services.AddAuthorization();
+
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IJwtProvider, JwtProvider>();
 builder.Services.AddHttpClient<IHelsinkiClient, HelsinkiClient>(client =>
     client.BaseAddress = new Uri("https://open-api.myhelsinki.fi/"));
-builder.Services.AddDbContext<PostgresContext>(options => 
+builder.Services.AddDbContext<PostgresContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresContext")));
 
 // Configure the HTTP request pipeline.
@@ -23,6 +48,8 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
