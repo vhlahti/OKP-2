286ef0abdb5fa36b1e2a5adcf32231f7b1b99ea5
Miconen <micropizza.mc@gmail.com>
2023-04-27 10:55:08 +0300

    Add new dbContext

---
+++ OKP-2-backend/Models/PostgresContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models;
+
+public partial class PostgresContext : DbContext
+{
+    public PostgresContext()
+    {
+    }
+
+    public PostgresContext(DbContextOptions<PostgresContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Favorite> Favorites { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        optionsBuilder.UseNpgsql("Host=db; Database=postgres; Username=postgres; Password=postgres", builder =>
+        {
+            builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+        });
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Favorite>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("favorites");
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(32)
+                .HasColumnName("id");
+            entity.Property(e => e.Type)
+                .HasMaxLength(16)
+                .HasColumnName("type");
+            entity.Property(e => e.User)
+                .HasMaxLength(16)
+                .HasColumnName("user");
+
+            entity.HasOne(d => d.UserNavigation).WithMany()
+                .HasForeignKey(d => d.User)
+                .HasConstraintName("favorites_user_fkey");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Name).HasName("pk_user");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(16)
+                .HasColumnName("name");
+            entity.Property(e => e.Password)
+                .HasMaxLength(64)
+                .HasColumnName("password");
+            entity.Property(e => e.Role)
+                .HasMaxLength(8)
+                .HasColumnName("role");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
