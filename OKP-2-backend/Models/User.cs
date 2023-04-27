2b703044a48ca989b532ad19ddb81ad1cda3cfdd
Miconen <micropizza.mc@gmail.com>
2023-04-27 10:56:34 +0300

    Add database models

--- OKP-2-backend/Models/User.cs
+++ OKP-2-backend/Models/User.cs
@@ -1,16 +1,10 @@
-namespace backend.Models;
+﻿namespace backend.Models;
 
 public partial class User
 {
-    public User(string name, string password)
-    {
-        Name = name;
-        Password = password;
-    }
-
     public string Name { get; set; } = null!;
 
     public string Password { get; set; } = null!;
 
-    public string Role { get; set; } = "user";
+    public string Role { get; set; } = "user"!;
 }
