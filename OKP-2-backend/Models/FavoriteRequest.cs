86fc9714ba92aaa12f48d0a0c10de618bbe35878
Miconen <micropizza.mc@gmail.com>
2023-04-28 10:27:45 +0300

    Add a favorite request model

---
+++ OKP-2-backend/Models/FavoriteRequest.cs
@@ -0,0 +1,10 @@
+namespace backend.Models;
+
+public partial class FavoriteRequest
+{
+    public string User { get; set; } = null!;
+
+    public string Type { get; set; } = null!;
+
+    public string Id { get; set; } = null!;
+}
