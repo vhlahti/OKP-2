f656a26602976ef1c0cfe71e0d6184e937383443
vhlahti <henri.veholahti@gmail.com>
2023-04-30 18:08:34 +0300

    add account service for forms support and authentication

--- OKP-2-frontend/src/app/app.component.ts
+++ OKP-2-frontend/src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'OKP-2-frontend';
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('user loaded');
+      }, error => {
+        console.log(error);
+      });
+  }
 
 }
