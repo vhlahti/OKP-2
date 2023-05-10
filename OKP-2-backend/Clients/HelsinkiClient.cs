79dc881a428fc61e6f94fe17920a27a5079362eb
Miconen <micropizza.mc@gmail.com>
2023-05-10 14:18:21 +0300

    Add GetOne for api endpoints that only fetch one entry

--- OKP-2-backend/Clients/HelsinkiClient.cs
+++ OKP-2-backend/Clients/HelsinkiClient.cs
@@ -1,3 +1,6 @@
+using System.Web;
+using backend.Models;
+
 namespace backend.Clients;
 
 // Source for this client pattern thing
@@ -11,13 +14,40 @@ public class HelsinkiClient : IHelsinkiClient
         _httpClient = httpClient;
     }
 
-    public async Task<string> Get(string address)
+    public async Task<string> Get(HelsinkiFilters filters, string address)
+    {
+        return await _httpClient.GetStringAsync(UriBuilder(filters, address));
+    }
+
+    public async Task<string> GetOne(string address)
     {
         return await _httpClient.GetStringAsync(address);
+    }
+
+    private string UriBuilder(HelsinkiFilters filters, string address)
+    {
+        // Append filters as query string parameters
+        var queryParams = new List<string>();
+        string url = address;
+
+        if (!string.IsNullOrEmpty(filters.limit)) queryParams.Add($"limit={HttpUtility.UrlEncode(filters.limit)}");
+        if (!string.IsNullOrEmpty(filters.start)) queryParams.Add($"start={HttpUtility.UrlEncode(filters.start)}");
+        if (!string.IsNullOrEmpty(filters.tags_search)) queryParams.Add($"tags_search={HttpUtility.UrlEncode(filters.tags_search)}");
+        if (!string.IsNullOrEmpty(filters.tags_filter)) queryParams.Add($"tags_filter={HttpUtility.UrlEncode(filters.tags_filter)}");
+        if (!string.IsNullOrEmpty(filters.distance_filter)) queryParams.Add($"distance_filter={HttpUtility.UrlEncode(filters.distance_filter)}");
+        if (!string.IsNullOrEmpty(filters.language_filter)) queryParams.Add($"language_filter={HttpUtility.UrlEncode(filters.language_filter)}");
+
+        if (queryParams.Any()) {
+            string queryString = string.Join("&", queryParams);
+            url += "?" + queryString;
+        }
+
+        return url;
     }
 }
 
 public interface IHelsinkiClient
 {
-    Task<string> Get(string address);
+    Task<string> Get(HelsinkiFilters filters, string address);
+    Task<string> GetOne(string address);
 }
