937db169044712da925b88aac0af80dcc48d255d
Kasper Keske <104263609+Kk4zu@users.noreply.github.com>
2023-05-17 21:34:59 +0300

    footer info done (#36)

--- OKP-2-frontend/src/app/components/footer/footer.component.ts
+++ OKP-2-frontend/src/app/components/footer/footer.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-  
+  faMapLocationDot = faMapLocationDot;
+  faGithub = faGithub;
+  showFooter: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    // Check if the user has scrolled to the bottom of the page
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    const windowHeight =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight ||
+      0;
+    const documentHeight =
+      document.documentElement.scrollHeight || document.body.scrollHeight || 0;
+
+    this.showFooter = scrollPosition + windowHeight >= documentHeight;
+  }
 }
