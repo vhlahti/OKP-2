080664c0df96b551ed695930fd8befee60751ccf
Miconen <micropizza.mc@gmail.com>
2023-05-19 00:34:50 +0300

    Fix environment variable file to point to production

--- OKP-2-frontend/src/app/services/account.service.ts
+++ OKP-2-frontend/src/app/services/account.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 interface IJWT {
     nameid: string;
