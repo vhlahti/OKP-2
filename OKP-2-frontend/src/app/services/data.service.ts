9fd32c5e476eabc3f8c4bba37cf7f8cca700650d
Mico <9155442+Miconen@users.noreply.github.com>
2023-05-19 10:52:35 +0300

    Add support for Railway hosting (#38)
    * Dockerfile testing
    
    * Change dockerfile port
    
    * Docker attempts at hosting
    
    * Change host
    
    * Change production host
    
    * Attempt at using env variables for backend url
    
    * Add node types
    
    * Fix production configuration
    
    * Fix production database connection
    
    * Fix environment variable file to point to production

--- OKP-2-frontend/src/app/services/data.service.ts
+++ OKP-2-frontend/src/app/services/data.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
-import { environment } from 'src/environments/environment.development';
 import { ApiTypes, APIFavoritesResponse, APIResponse } from 'src/app/models/IApiResponse';
 import { ActivityV2 } from 'src/app/models/helsinki-api-model';
 import { Event } from 'src/app/models/helsinki-api-model';
@@ -10,6 +9,7 @@ import { PlaceV2 } from 'src/app/models/helsinki-api-model';
 import { ILocation } from 'src/app/models/ILocation';
 import { GoogleMap } from '@angular/google-maps';
 import { AccountService } from './account.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
